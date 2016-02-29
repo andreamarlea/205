0af6e2fc915a5afa94291f3a66d23e7a4ba50e8a
andreamarlea <andreamarlea@gmail.com>
2016-02-29 08:22:35 +0800

    Update Hopsital_Variability.sql

--- Exercise_1/Investigations/Hospital_Variability/Hopsital_Variability.sql
+++ Exercise_1/Investigations/Hospital_Variability/Hopsital_Variability.sql
@@ -6,6 +6,7 @@ max(score) as max_score,
 min(score) as min_score,
 max(score) - min(score) as score_variation
 from care_new
+where score IS NOT NULL and measure_name IS NOT NULL
 group by measure_ID, measure_name
 order by score_variation desc
 limit 10;
