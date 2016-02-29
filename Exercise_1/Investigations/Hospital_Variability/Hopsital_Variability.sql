2b53885f99c5e17559a4ae9bdd8b8ab66044397c
andreamarlea <andreamarlea@gmail.com>
2016-02-29 08:35:29 +0800

    Update Hopsital_Variability.sql

--- Exercise_1/Investigations/Hospital_Variability/Hopsital_Variability.sql
+++ Exercise_1/Investigations/Hospital_Variability/Hopsital_Variability.sql
@@ -1,4 +1,4 @@
---hospital variability
+--Hospital Variability
 select measure_ID, 
 measure_name, 
 avg(score) as average_score, 
@@ -7,6 +7,8 @@ min(score) as min_score,
 max(score) - min(score) as score_variation
 from care_new
 where score IS NOT NULL and measure_name IS NOT NULL
+and score <> 'Not Available'
 group by measure_ID, measure_name
 order by score_variation desc
 limit 10;
+
